ba4cf84c4a819c405928d6d0b2cb20ea3576249e
张泽 <zhangzeStrong@126.com>
2017-05-24 13:56:48 +0800

    项目初始化

---
+++ ZZBLEGlight/ZZBLEGlight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZBLEGlight
+//
+//  Created by yaoln on 2017/5/24.
+//  Copyright © 2017年 zhangze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZBLEGlight/ZZBLEGlight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZZBLEGlight
+//
+//  Created by yaoln on 2017/5/24.
+//  Copyright © 2017年 zhangze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
